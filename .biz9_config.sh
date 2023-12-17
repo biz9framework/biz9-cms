5efbff72f2510874cf9ffadf50bad3e53b13f2e5
tank <tank@certifiedcoderz.com>
2023-12-16 19:48:28 -0500

    updated scriptz app_info file.

--- .biz9_config.sh
+++ .biz9_config.sh
@@ -1,11 +1,11 @@
 BIZ9_CMS_VERSION='6.6.8'
 APP_VERSION='1.5.2'
-APP_ID='19';
+PROJECT_ID='19';
 APP_TITLE='BiZ9-CMS';
 APP_TITLE_ID='cms19';
-EC2_IP_ADDR="0.00.000.000"
-EC2_DEPLOY_DIR="/home/admin/www/cms-_app_id_"
-EC2_KEY_FILE="other/aws/ec2_key/_app_id_.pem"
+SERVER_IP_ADDR="0.00.000.000"
+SERVER_DEPLOY_DIR="/home/admin/www/cms-_app_id_"
+SSH_KEY="other/aws/ec2_key/_app_id_.pem"
 REPO_URL="${BIZ9_GIT_URL}biz9-cms.git";
 MONGO_PORT='27019';
 
