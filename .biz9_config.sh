8d290f4535a10203ce86f12a1e18643b53d0081f
tank <tank@certifiedcoderz.com>
2023-12-27 20:48:49 -0500

    good

--- .biz9_config.sh
+++ .biz9_config.sh
@@ -1,11 +1,9 @@
-BIZ9_CMS_VERSION='6.9.7'
+BIZ9_CMS_VERSION='7.0.2'
 APP_VERSION='1.0.0'
 PROJECT_ID='19';
 APP_TITLE='BiZ9-CMS';
 APP_TITLE_ID='cms19';
 SERVER_IP="0.00.000.000"
-SERVER_DEPLOY_DIR="/home/admin/www/cms-_app_id_"
+SERVER_DIR="/home/admin/www/cms-_app_id_"
 SSH_KEY="other/aws/ec2_key/_app_id_.pem"
-REPO_URL="${BIZ9_GIT_URL}biz9-cms.git";
-MONGO_PORT='27019';
-
+GIT_REPO="${BIZ9_GIT_REPO}biz9-cms.git";
